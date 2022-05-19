a6cee142169862fc01daf373ff9d9ec21f797c18
Riteshpise <86122284+Riteshpise@users.noreply.github.com>
2022-05-20 03:56:58 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/Riteshpise'
+      className='link footer__link'
+    >
+      Created By Ritesh, Rudra, Chaitanya (RRih)
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,69 @@
+const header = {
+    // all the properties are optional - can be left empty or deleted
+    homepage: 'https://rrih-bot.web.app/.js',
+    title: 'RRih.',
+  }
+  
+  const about = {
+    // all the properties are optional - can be left empty or deleted
+    name: 'RRih ',
+    role: 'Your Teaching Assistant',
+    description:
+      'Hey there Welcome to my HOME. I am your on the go Virtual Teaching Assistant. You can call me anytime anywhere I will be there for your help. You can rely on me for your any academic related queries. I will provide you the most accurate explaination/answers of your questions. I am also available on the different social media platforms like Telegram and Whatsapp, we can also meet there...  OOH! I forgot Im here to help you, So Discover what I can do. ',
+    resume: 'App.js',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/ritesh-pise-793b621a2/',
+      github: 'https://github.com/Riteshpise',
+    },
+  }
+  
+  const projects = [
+    // projects can be added an removed
+    // if there are no projects, Projects section won't show up
+    {
+      name: 'Telegram',
+      description:
+        'Hey I am also available on Telegram check me out here.../t.me/RRih_bot',
+      stack: ['TELEGRAM', 'RRih_bot', 'TELEGRAM'],
+      sourceCode: 'https://github.com',
+      livePreview: 'https://telegram.org',
+    },
+    {
+      name: 'WhatsApp',
+      description:
+        'Hey I am also available on WhatsApp check me out here...',
+      stack: ['WhatsApp', 'React'],
+      sourceCode: 'https://github.com',
+      livePreview: 'https://www.whatsapp.com/',
+    },
+    {
+      name: 'Facebook Messenger',
+      description:
+        'Hey I am also available on Facebook Messenger check me out here...',
+      stack: ['Facebook', 'Messenger', 'React'],
+      sourceCode: 'https://github.com',
+      livePreview: 'https://facebook.com',
+    },
+  ]
+  
+  const skills = [
+    // skills can be added or removed
+    // if there are no skills, Skills section won't show up
+    'Customized interaction',
+    'Emotional intelligence',
+    'Integration with 3rd party apps',
+    'Smooth Conversation',
+    'Easy live chat',
+    'Security & Privacy',
+    'Context aware',
+    'in your control',
+    
+  ]
+  
+  const contact = {
+    // email is optional - if left empty Contact section won't show up
+    email: 'riteshpise2@mail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
+  
